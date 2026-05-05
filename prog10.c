49f1d325b30dcc43a91c9becc688395bae6b6567
Alexandr Karpov <sashakarpov29042007@gmail.com>
2026-05-05 16:24:06 +0300

    убрали конфликт и изменили переменную d на массив

--- prog10.c
+++ prog10.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
-<<<<<<< HEAD
-int main(){
-	int a1,b,c,d;
-	a1 = 10;
-	b = 5;
-	c = 7;
-	d = 10;
-	printf(a1+b+c+d);
-=======
 int main() {
 	int a1;
 	int b;
 	int c;
-	int d;
+	int d = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
 	a1 = 10;
 	b = 7;
 	c = 8;
-	d = 10;
 	printf(a1 + b + c + d);
->>>>>>> 17cf0b7a876599a716a988606514646dbb593dcb
 }
